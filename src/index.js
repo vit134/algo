c5b2dd3ce5f01558508d4e9f672a7dac54d94d05
Vitaliy Andryushkov <vit134@mail.ru>
2018-06-05 18:21:12 +0300

    promise

--- src/index.js
+++ src/index.js
@@ -243,4 +243,119 @@ function correctBrackets(str) {
     return result;
 }
 
-console.log(correctBrackets('((]){]'));
+//console.log(correctBrackets('((]){]'));
+
+
+// own promis
+
+class OwnPromise {
+    constructor(func) {
+        this.chain = [];
+        this.errorHandler = () => {};
+
+        this.onResolve = this.onResolve.bind(this);
+        this.onReject = this.onReject.bind(this);
+
+        func(this.onResolve, this.onReject);
+    }
+
+    then(onResolve) {
+        this.chain.push(onResolve);
+
+        return this;
+    }
+
+    catch(errorHandler) {
+        this.errorHandler = errorHandler;
+
+        return this;
+    }
+
+    onResolve(value) {
+        let storedValue = value;
+
+        try {
+            this.chain.forEach(nextFunc => {
+                storedValue = nextFunc(storedValue);
+            })
+        } catch (err) {
+            this.chain = [];
+
+            this.onReject(err);
+        }
+    }
+
+    onReject(err) {
+        this.errorHandler(err);
+    }
+}
+
+const fakeApiBackend = () => {
+    const user = {
+      username: 'treyhuffine',
+      favoriteNumber: 42,
+      profile: 'https://gitconnected.com/treyhuffine'
+    };
+  
+    // Introduce a randomizer to simulate the
+    // the probability of encountering an error
+    if (Math.random() > .05) {
+      return { 
+        data: user, 
+        statusCode: 200,
+      };
+    } else {
+      const error = {
+        statusCode: 404,
+        message: 'Could not find user',
+        error: 'Not Found',
+      };
+      
+      return error;
+    }
+  };
+
+  const makeApiCall = () => {
+    return new OwnPromise((resolve, reject) => {
+      // Use a timeout to simulate the network delay waiting for the response.
+      // This is THE reason you use a promise. It waits for the API to respond
+      // and after received, it executes code in the `then()` blocks in order.
+      // If it executed is immediately, there would be no data.
+      setTimeout(() => {
+        const apiResponse = fakeApiBackend();
+  
+        if (apiResponse.statusCode >= 400) {
+          reject(apiResponse);
+        } else {
+          resolve(apiResponse.data);
+        }
+      }, 5000);
+    });
+  };
+
+
+  makeApiCall()
+  .then((user) => {
+    console.log('In the first .then()');
+  
+    return user;
+  })
+  .then((user) => {
+    console.log(`User ${user.username}'s favorite number is ${user.favoriteNumber}`);
+  
+    return user;
+  })
+  .then((user) => {
+    console.log('The previous .then() told you the favoriteNumber')
+  
+    return user.profile;
+  })
+  .then((profile) => {
+    console.log(`The profile URL is ${profile}`);
+  })
+  .then(() => {
+    console.log('This is the last then()');
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
